aec3a9fcc5880378fc53c849ee2e3bead88741c4
Karmina <gusdbf1114@Gmail.com>
2024-10-21 00:14:26 +0900

    init

---
+++ idmvton/gradio_demo/densepose/modeling/predictors/registry.py
@@ -0,0 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from idmvton.gradio_demo.detectron2.utils.registry import Registry
+
+DENSEPOSE_PREDICTOR_REGISTRY = Registry("DENSEPOSE_PREDICTOR")
